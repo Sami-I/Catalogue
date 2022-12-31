4d0b96843f3c4d33d53a89503362e47993a39c2d
Sami <sami_khalid@hotmail.co.uk>
2022-12-31 22:45:24 +0000

    updated maylike

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -59,9 +59,14 @@ const ProductDetails = ({ product, products }) => {
         <h2>You may also like</h2>
         <div className="marquee">
           <div className="maylike-products-container track">
-            {products.map((item) => (
-              <Product key={item._id} product={item} />
-            ))}
+            {products.map((item) =>
+              item.category === product.category ||
+              item.gender === product.category ? (
+                <Product key={item._id} product={item} />
+              ) : (
+                <></>
+              )
+            )}
           </div>
         </div>
       </div>
